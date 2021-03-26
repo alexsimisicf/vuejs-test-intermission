d8fe042e5e638145ee244105a232592cb4ef78fb
Alex Simisi Nta <alex.simisinta@connecting-food.com>
2021-03-26 10:06:09 +0100

    mise en place de la barre de navigation responsive

--- src/App.vue
+++ src/App.vue
@@ -42,15 +42,4 @@ body {
     width: 100%;
   }
 }
-
-#nav {
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/components/layouts/navbar/NavBar.vue
+++ src/components/layouts/navbar/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
   <nav class="nav">
+    <input type="checkbox" id="nav-toggle" />
     <div class="logo">
       <img
         alt="Connecting Food logo"
@@ -35,12 +36,12 @@
   </nav>
 </template>
 
-<style scoped lang="scss">
+<style scoped lang="css">
 img {
   width: 100px;
 }
 
-.nav {
+nav {
   position: fixed;
   z-index: 10;
   left: 0;
@@ -48,55 +49,118 @@ img {
   top: 0;
   height: 100px;
   padding: 0 5%;
-  color: #071f3a;
+  color: #074fa0;
   background-color: #f1f3f5;
-  .logo {
-    float: left;
-    width: 40%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    font-size: 24px;
-  }
-  .links {
-    float: right;
-    margin: 0;
-    padding: 0;
-    width: 60%;
-    height: 100%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    li {
-      list-style: none;
-    }
-    a {
-      display: block;
-      padding: 1em;
-      font-size: 12px;
-      font-weight: 900;
-      color: #071f3a;
-      text-decoration: none;
+}
 
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
+nav .logo {
+  float: left;
+  width: 40%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 24px;
+}
+
+nav .links {
+  float: right;
+  margin: 0;
+  padding: 0;
+  width: 60%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+nav .links li {
+  list-style: none;
+}
+
+nav .links a {
+  display: block;
+  padding: 1em;
+  font-size: 12px;
+  font-weight: 900;
+  color: #074fa0;
+  text-decoration: none;
+}
+
+nav .links a.router-link-exact-active {
+  color: #1c5d41;
+  text-decoration: 3px underline;
+}
+
+#nav-toggle {
+  position: absolute;
+  top: -100px;
+}
+
+nav .icon-burger {
+  display: none;
+  position: absolute;
+  right: 5%;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+nav .icon-burger .line {
+  width: 30px;
+  height: 5px;
+  background-color: #074fa0;
+  margin: 5px;
+  border-radius: 3px;
+  transition: all 0.3s ease-in-out;
+}
+
+@media screen and (max-width: 768px) {
+  nav .logo {
+    float: none;
+    width: auto;
+    justify-content: center;
   }
-  .icon-burger {
-    display: none;
-    position: absolute;
-    right: 5%;
-    top: 50%;
-    transform: translateY(-50%);
-    .line {
-      width: 30px;
-      height: 5px;
-      background-color: #071f3a;
-      margin: 5px;
-      border-radius: 3px;
-      transition: all 0.3s ease-in-out;
-    }
+
+  nav .links {
+    float: none;
+    position: fixed;
+    z-index: 9;
+    left: 0;
+    right: 0;
+    top: 100px;
+    bottom: 100%;
+    flex-direction: column;
+    justify-content: space-evenly;
+    background-color: rgba(7, 79, 160, 0.8);
+    width: auto;
+    height: auto;
+    overflow: hidden;
+    transition: all 0.5s ease-in-out;
+  }
+
+  nav .links a {
+    color: #f1f3f5;
+    font-size: 15px;
+  }
+
+  nav :checked ~ .links {
+    bottom: 0;
+  }
+
+  nav .icon-burger {
+    display: block;
+    cursor: pointer;
+  }
+
+  nav :checked ~ .icon-burger .line:nth-child(1) {
+    transform: translateY(10px) rotate(225deg);
+  }
+
+  nav :checked ~ .icon-burger .line:nth-child(3) {
+    transform: translateY(-10px) rotate(-225deg);
+  }
+
+  nav :checked ~ .icon-burger .line:nth-child(2) {
+    opacity: 0;
   }
 }
 </style>
